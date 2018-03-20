bfd0daef6cf1d3fbe8479425a1c6dbbb88cecbcc
faisalahamedJ <37343326+faisalahamedJ@users.noreply.github.com>
2018-03-20 23:36:09 +0530

    Update Largest of three number.py

--- Beginner Level/Largest of three number.py
+++ Beginner Level/Largest of three number.py
@@ -4,8 +4,8 @@ num3 = input("Enter third number\n")
 if(num1>=num2):
     if(num1>=num3):
         print(num1, "is Largest")
-	  else:
-		    print(num3, "is largest")
+    else:
+        print(num3, "is largest")
 elif(num2>=num3):
 	      print(num2, "is Largest")
 else:
