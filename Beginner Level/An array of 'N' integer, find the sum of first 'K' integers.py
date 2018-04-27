d39985b96b0721b4e1156a23cc8b70d55157cfdf
faisalahamedJ <37343326+faisalahamedJ@users.noreply.github.com>
2018-04-27 10:34:12 +0530

    Update An array of 'N' integer, find the sum of first 'K' integers.py

--- Beginner Level/An array of 'N' integer, find the sum of first 'K' integers.py
+++ Beginner Level/An array of 'N' integer, find the sum of first 'K' integers.py
@@ -1,13 +1,11 @@
 from array import *
-N = input("Enter the number of value in array")
+N = input()
 my_array =[]
-print("Enter the value")
 for i in range (0,N) :
     x = input()
     my_array.append(x)
-print("Enter how many number to add")
-k = input()
+K = input()
 add = 0
-for s in range (0,k):
+for s in range (0,K):
     add = add + my_array[s]
 print(add)
