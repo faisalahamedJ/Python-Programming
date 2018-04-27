c514235f86bc4d8a006359c1a088fc5f02f6b2ff
faisalahamedJ <37343326+faisalahamedJ@users.noreply.github.com>
2018-04-27 12:15:36 +0530

    Update Even numbers between two intervals.py

--- Beginner Level/Even numbers between two intervals.py
+++ Beginner Level/Even numbers between two intervals.py
@@ -1,5 +1,7 @@
-a = input()
-b = input()
-for i in range(a+1,b):
-    if(i%2 == 0):
+a = []
+b = raw_input()
+a = b.split(" ")
+
+for i in range(int(a[0])+1,int(a[1])):
+    if(i % 2 == 0):
         print i,
